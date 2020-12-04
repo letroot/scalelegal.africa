86f6764c07b454bb8ebb2c537d7785a9aa55f2b4
noble <noblewill2012@icloud.com>
2020-12-04 08:19:57 +0100

    header for mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,17 +2,48 @@
   <div
     class="sm:px-10 xl:px-16 sm:py-8 sm:flex sm:items-center sm:justify-between"
   >
-    <div class="sm:w-4/12 lg:w-5/12 xl:w-6/12">
-      <img src="../assets/img/logo.svg" alt="logo" class="sm:h-12 xl:h-16" />
-    </div>
     <div
-      class="sm:w-8/12 lg:w-7/12 xl:w-7/12 sm:flex sm:justify-between sm:items-center"
+      class="flex justify-between items-center p-4 sm:w-4/12 lg:w-5/12 xl:w-6/12"
     >
-      <a href="#" class="sm:text-xs xl:text-base xl:leading-none text-nav-text">Our Services</a>
-      <a href="#" class="sm:text-xs xl:text-base xl:leading-none text-nav-text">Expertise</a>
-      <a href="#" class="sm:text-xs xl:text-base xl:leading-none text-nav-text">The Team</a>
-      <a href="#" class="sm:text-xs xl:text-base xl:leading-none text-nav-text">Blog</a>
+      <div class="w-6/12 sm:w-full">
+        <img src="../assets/img/logo.svg" alt="logo" class="sm:h-12 xl:h-16" />
+      </div>
+      <button class="focus:outline-none sm:hidden" @click="toggle">
+        <img
+          src="../assets/img/close.svg"
+          alt="logo"
+          class="h-8 p-1"
+          v-if="isopen"
+        />
+        <img src="../assets/img/menu.svg" alt="logo" class="h-5" v-else />
+      </button>
+    </div>
+
+    <div
+      class="sm:w-8/12 lg:w-7/12 xl:w-7/12 sm:flex sm:flex-row sm:justify-between sm:items-center p-4 sm:p-0 flex flex-col text-right"
+      :class="{ hidden: !isopen, flex: isopen }"
+    >
       <a href="#" class="sm:text-xs xl:text-base xl:leading-none text-nav-text"
+        >Our Services</a
+      >
+      <a
+        href="#"
+        class="sm:text-xs xl:text-base xl:leading-none text-nav-text mt-4 sm:mt-0"
+        >Expertise</a
+      >
+      <a
+        href="#"
+        class="sm:text-xs xl:text-base xl:leading-none text-nav-text mt-4 sm:mt-0"
+        >The Team</a
+      >
+      <a
+        href="#"
+        class="sm:text-xs xl:text-base xl:leading-none text-nav-text mt-4 sm:mt-0"
+        >Blog</a
+      >
+      <a
+        href="#"
+        class="sm:text-xs xl:text-base xl:leading-none text-nav-text mt-4 sm:mt-0"
         ><button
           class="border border-nav-text rounded-xl py-2 sm:py-3 px-3 focus:outline-none"
         >
@@ -24,5 +55,16 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isopen: false,
+    };
+  },
+  methods: {
+    toggle() {
+      this.isopen = !this.isopen;
+    },
+  },
+};
 </script>
