bc6f29b8a3d085ab460e0d95777110279213f17a
noble <noblewill2012@icloud.com>
2020-12-05 23:31:52 +0100

    xl screen adjustments

--- src/App.vue
+++ src/App.vue
@@ -36,10 +36,7 @@
     </div>
 
     <div class="mt-20 sm:mt-32 lg:mt-48 sm:px-12 lg:px-24 xl:px-36">
-      <div
-        class="flex bg-bg sm:shadow-xl sm:rounded-xl px-10 sm:px-7 py-8
-                  xl:flex"
-      >
+      <div class="flex bg-bg sm:shadow-xl sm:rounded-xl px-10 sm:px-7 py-8">
         <div class="w-6/12 sm:w-5/12">
           <img src="./assets/img/slant-books.png" alt="book" />
         </div>
@@ -49,7 +46,7 @@
             sm:text-3xl sm:w-3/4 ml-5 sm:ml-12 lg:ml-32 mt-3 sm:mt-8
             sm:leading-tight
             lg:mt-20 lg:text-4xl
-            xl:text-5xl xl:w-7/12
+            xl:text-5xl
             archivo-font"
           >
             The 20-page Book <br class="hidden sm:block" />
@@ -59,24 +56,24 @@
       </div>
     </div>
 
-    <div class="mt-20 sm:mt-32 lg:mt-48 sm:px-12 lg:px-24 sm:flex px-10">
+    <div class="mt-20 sm:mt-32 lg:mt-48 sm:px-12 lg:px-24 xl:px-36 sm:flex px-10">
       <div class="sm:w-7/12">
         <p
-          class="text-center sm:text-left sm:text-lg lg:text-2xl xl:text-5xl sm:w-3/4 sm:mt-8 lg:mt-10 text-body-text sm:leading-tight archivo-font"
+          class="text-center sm:text-left sm:text-lg lg:text-2xl xl:text-4xl sm:w-3/4 sm:mt-8 lg:mt-10 text-body-text sm:leading-tight archivo-font"
         >
           Copy about what the book is good for, consectetur adipiscing elit, sed
           do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
           ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat.
         </p>
-        <div class="mt-8 sm:mt-16 text-center sm:text-left">
+        <div class="mt-8 sm:mt-16 xl:mt-20 text-center sm:text-left">
           <button
-            class="bg-scale-green py-2 rounded-lg px-5 focus:outline-none text-white lg:text-lg"
+            class="bg-scale-green py-2 rounded-lg px-5 focus:outline-none text-white lg:text-lg xl:text-2xl"
           >
             Buy eBook Now
           </button>
           <button
-            class="bg-nav-text py-2 rounded-lg px-5 ml-3 focus:outline-none text-white lg:text-lg"
+            class="bg-nav-text py-2 rounded-lg px-5 ml-3 focus:outline-none text-white lg:text-lg xl:text-2xl"
           >
             Learn More
           </button>
@@ -87,51 +84,51 @@
       </div>
     </div>
 
-    <div class="sm:px-20 lg:mt-48 lg:px-24 sm:mt-32 mt-10 px-10">
-      <h2 class="text-center text-gray-text archivo-font sm:text-xl uppercase">
+    <div class="sm:px-20 lg:mt-48 lg:px-24 xl:px-36 sm:mt-32 mt-10 px-10">
+      <h2 class="text-center text-gray-text archivo-font sm:text-xl xl:text-2xl uppercase">
         scalelegal is trusted by
       </h2>
       <div
         class="mt-6 sm:mt-10 text-center archivo-font text-body-text sm:w-9/12 sm:mx-auto"
       >
-        <p class="sm:text-base text-sm">
+        <p class="sm:text-base xl:text-xl text-sm">
           Trusted by more than 10 companies in various sectors. Our Team has
           over 10 years experience in corporate legal contexts.
         </p>
       </div>
     </div>
 
-    <div class="sm:mt-32 lg:mt-48 lg:px-24 sm:px-12 sm:flex mt-20 px-10">
+    <div class="sm:mt-32 lg:mt-48 lg:px-24 xl:px-36 sm:px-12 sm:flex mt-20 px-10">
       <div class="sm:w-7/12">
         <h1
-          class="text-center sm:text-left text-xl sm:text-3xl lg:text-4xl text-nav-text"
+          class="text-center sm:text-left text-xl sm:text-3xl lg:text-4xl xl:text-5xl text-nav-text"
         >
           What is Covered in the Book
         </h1>
         <div
-          class="sm:text-xl lg:text-2xl xl:text-5xl sm:w-10/12 mt-6 sm:mt-8 lg:mt-16 text-body-text sm:leading-tight archivo-font"
+          class="sm:text-xl lg:text-2xl xl:text-3xl sm:w-10/12 mt-6 sm:mt-8 lg:mt-16 text-body-text sm:leading-tight archivo-font"
         >
-          <div class="flex sm:h-10 lg:h-12 h-7 items-center">
+          <div class="flex sm:h-10 lg:h-12 xl:h-14 h-7 items-center">
             <img src="./assets/img/bullet.svg" alt="bullet" class="h-3" />
             <p class="ml-4">Quis nostrud exercitation ullamco</p>
           </div>
-          <div class="flex sm:h-10 lg:h-12 h-7 items-center">
+          <div class="flex sm:h-10 lg:h-12 xl:h-14 h-7 items-center">
             <img src="./assets/img/bullet.svg" alt="bullet" class="h-3" />
             <p class="ml-4">Quis nostrud exercitation ullamco</p>
           </div>
-          <div class="flex sm:h-10 lg:h-12 h-7 items-center">
+          <div class="flex sm:h-10 lg:h-12 xl:h-14 h-7 items-center">
             <img src="./assets/img/bullet.svg" alt="bullet" class="h-3" />
             <p class="ml-4">Quis nostrud exercitation ullamco</p>
           </div>
-          <div class="flex sm:h-10 lg:h-12 h-7 items-center">
+          <div class="flex sm:h-10 lg:h-12 xl:h-14 h-7 items-center">
             <img src="./assets/img/bullet.svg" alt="bullet" class="h-3" />
             <p class="ml-4">Quis nostrud exercitation ullamco</p>
           </div>
-          <div class="flex sm:h-10 lg:h-12 h-7 items-center">
+          <div class="flex sm:h-10 lg:h-12 xl:h-14 h-7 items-center">
             <img src="./assets/img/bullet.svg" alt="bullet" class="h-3" />
             <p class="ml-4">Quis nostrud exercitation ullamco</p>
           </div>
-          <div class="flex sm:h-10 lg:h-12 h-7 items-center">
+          <div class="flex sm:h-10 lg:h-12 xl:h-14 h-7 items-center">
             <img src="./assets/img/bullet.svg" alt="bullet" class="h-3" />
             <p class="ml-4">Quis nostrud exercitation ullamco</p>
           </div>
@@ -142,70 +139,70 @@
       </div>
     </div>
 
-    <div class="sm:px-20 sm:mt-32 lg:mt-48 lg:px-24 mt-20 px-10">
+    <div class="sm:px-20 sm:mt-32 lg:mt-48 lg:px-24 xl:px-36 mt-20 px-10">
       <h2
-        class="text-center text-nav-text archivo-font text-xl sm:text-3xl lg:text-4xl sm:w-9/12 sm:mx-auto"
+        class="text-center text-nav-text archivo-font text-xl sm:text-3xl lg:text-4xl xl:text-5xl sm:w-9/12 sm:mx-auto"
       >
         Learn all you need to make your business ready to scale.
       </h2>
       <div
-        class="sm:mt-8 lg:mt-12 mt-5 text-center archivo-font sm:w-9/12 sm:mx-auto"
+        class="sm:mt-8 lg:mt-12 xl:mt-16 mt-5 text-center archivo-font sm:w-9/12 sm:mx-auto"
       >
         <button
-          class="bg-scale-green py-2 rounded-lg px-5 focus:outline-none text-white"
+          class="bg-scale-green py-2 rounded-lg px-5 focus:outline-none text-white xl:text-2xl"
         >
           Download book now
         </button>
       </div>
     </div>
 
-    <div class="sm:px-20 sm:mt-32 lg:mt-48 lg:px-24 mt-20 px-10">
+    <div class="sm:px-20 sm:mt-32 lg:mt-48 lg:px-24 xl:px-36 mt-20 px-10">
       <h2
-        class="text-center text-nav-text archivo-font text-xl sm:text-3xl lg:text-4xl sm:w-9/12 sm:mx-auto"
+        class="text-center text-nav-text archivo-font text-xl sm:text-3xl lg:text-4xl xl:text-5xl sm:w-9/12 sm:mx-auto"
       >
         Why You Should Learn...
       </h2>
       <div
         class="sm:mt-8 mt-5 text-center archivo-font text-body-text sm:w-9/12 sm:mx-auto"
       >
-        <p class="sm:text-base text-sm">
-          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        <p class="sm:text-base text-sm xl:text-xl">
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut <br class="hidden xl:block">
           labore et dolore magna aliqua. Ut
         </p>
       </div>
-      <div class="flex flex-col sm:flex-row mt-8 sm:mt-12">
-        <div class="sm:w-1/3 flex flex-col px-2">
+      <div class="flex flex-col sm:flex-row mt-8 xl:mt-20 sm:mt-12">
+        <div class="sm:w-1/3 flex flex-col px-2 xl:px-6">
           <img
             src="./assets/img/growth.svg"
             alt="growth"
-            class="sm:h-32 h-48"
+            class="sm:h-32 xl:h-48 h-48"
           />
-          <h2 class="text-lg sm:text-xl text-center">Secure Biz</h2>
-          <p class="text-body-text text-center mt-3 leading-tight text-sm">
+          <h2 class="text-lg sm:text-xl xl:text-3xl xl:mt-5 text-center">Secure Biz</h2>
+          <p class="text-body-text text-center mt-3 leading-tight text-sm xl:text-lg">
             consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
             labore et dolore magna aliqua. Ut
           </p>
         </div>
-        <div class="sm:w-1/3 flex flex-col px-2 mt-10 sm:mt-0">
+        <div class="sm:w-1/3 flex flex-col px-2 xl:px-6 mt-10 sm:mt-0">
           <img
             src="./assets/img/growth.svg"
             alt="growth"
-            class="sm:h-32 h-46"
+            class="sm:h-32 h-48 xl:h-48"
           />
-          <h2 class="text-lg sm:text-xl text-center">Secure Biz</h2>
-          <p class="text-body-text text-center mt-3 leading-tight text-sm">
+          <h2 class="text-lg sm:text-xl xl:text-3xl xl:mt-5 text-center">Secure Biz</h2>
+          <p class="text-body-text text-center mt-3 leading-tight text-sm xl:text-lg">
             consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
             labore et dolore magna aliqua. Ut
           </p>
         </div>
-        <div class="sm:w-1/3 flex flex-col px-2 mt-10 sm:mt-0">
+        <div class="sm:w-1/3 flex flex-col px-2 xl:px-6 mt-10 sm:mt-0">
           <img
             src="./assets/img/growth.svg"
             alt="growth"
-            class="sm:h-32 h-46"
+            class="sm:h-32 h-48 xl:h-48"
           />
-          <h2 class="text-lg sm:text-xl text-center">Secure Biz</h2>
-          <p class="text-body-text text-center mt-3 leading-tight text-sm">
+          <h2 class="text-lg sm:text-xl xl:text-3xl xl:mt-5 text-center">Secure Biz</h2>
+          <p class="text-body-text text-center mt-3 leading-tight text-sm xl:text-lg">
             consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
             labore et dolore magna aliqua. Ut
           </p>
@@ -213,28 +210,28 @@
       </div>
     </div>
 
-    <div class="sm:px-20 sm:mt-32 lg:mt-48 lg:px-24 px-10 mt-20">
+    <div class="sm:px-20 sm:mt-32 lg:mt-48 lg:px-24 xl:px-36 px-10 mt-20">
       <h2
-        class="text-center text-nav-text archivo-font text-xl sm:text-3xl lg:text-4xl sm:w-9/12 sm:mx-auto"
+        class="text-center text-nav-text archivo-font text-xl sm:text-3xl lg:text-4xl xl:text-5xl sm:w-9/12 sm:mx-auto"
       >
         What Our Clients Think
       </h2>
       <div
         class="sm:mt-8 mt-5 text-center archivo-font text-body-text sm:text-xl sm:w-9/12 sm:mx-auto"
       >
-        <p class="sm:text-base text-sm">
-          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        <p class="sm:text-base text-sm xl:text-xl">
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut <br class="hidden xl:block">
           labore et dolore magna aliqua. Ut
         </p>
       </div>
-      <div class="flex flex-wrap sm:mt-16 mt-12">
+      <div class="flex flex-wrap sm:mt-16 xl:mt-24 mt-12">
         <div
-          class="rounded-md w-2/3 sm:w-5/12 xl:w-64 shadow-xl px-4 pb-5 lg:py-10 mx-auto bg-client-bg"
+          class="rounded-md w-2/3 sm:w-5/12 xl:w-1/5 shadow-xl px-4 pb-5 lg:py-10 mx-auto bg-client-bg"
         >
           <div
             class="border-2 border-profile-border rounded-full w-12 h-12 xl:w-16 xl:h-16 -mt-6 xl:-mt-16 bg-white mx-auto"
           ></div>
-          <p class="text-xs text-center text-white mt-4 ">
+          <p class="text-xs xl:text-sm text-center text-white mt-4 ">
             AutoCare is my go to for all my car maintenance. I’ve saved a lot of
             money that I would have lost at a mechanic’s shop.
           </p>
@@ -243,12 +240,12 @@
           </h3>
         </div>
         <div
-          class="rounded-md w-2/3 sm:w-5/12 xl:w-64 shadow-xl px-4 pb-5 lg:py-10 mx-auto bg-client-bg mt-14 sm:mt-0"
+          class="rounded-md w-2/3 sm:w-5/12 xl:w-1/5 shadow-xl px-4 pb-5 lg:py-10 mx-auto bg-client-bg mt-14 sm:mt-0"
         >
           <div
             class="border-2 border-profile-border rounded-full w-12 h-12 xl:w-16 xl:h-16 -mt-6 xl:-mt-16 bg-white mx-auto"
           ></div>
-          <p class="text-xs text-center text-white mt-4 ">
+          <p class="text-xs xl:text-sm text-center text-white mt-4 ">
             AutoCare is my go to for all my car maintenance. I’ve saved a lot of
             money that I would have lost at a mechanic’s shop.
           </p>
@@ -257,12 +254,12 @@
           </h3>
         </div>
         <div
-          class="rounded-md w-2/3 sm:w-5/12 xl:w-64 shadow-xl px-4 pb-5 lg:py-10 mx-auto bg-client-bg mt-14 sm:mt-12"
+          class="rounded-md w-2/3 sm:w-5/12 xl:w-1/5 shadow-xl px-4 pb-5 lg:py-10 mx-auto bg-client-bg mt-14 sm:mt-12 xl:mt-0"
         >
           <div
             class="border-2 border-profile-border rounded-full w-12 h-12 xl:w-16 xl:h-16 -mt-6 xl:-mt-16 bg-white mx-auto"
           ></div>
-          <p class="text-xs text-center text-white mt-4 ">
+          <p class="text-xs xl:text-sm text-center text-white mt-4 ">
             AutoCare is my go to for all my car maintenance. I’ve saved a lot of
             money that I would have lost at a mechanic’s shop.
           </p>
@@ -271,12 +268,12 @@
           </h3>
         </div>
         <div
-          class="rounded-md w-2/3 sm:w-5/12 xl:w-64 shadow-xl px-4 pb-5 lg:py-10 mx-auto bg-client-bg mt-14 sm:mt-12"
+          class="rounded-md w-2/3 sm:w-5/12 xl:w-1/5 shadow-xl px-4 pb-5 lg:py-10 mx-auto bg-client-bg mt-14 sm:mt-12 xl:mt-0"
         >
           <div
             class="border-2 border-profile-border rounded-full w-12 h-12 xl:w-16 xl:h-16 -mt-6 xl:-mt-16 bg-white mx-auto"
           ></div>
-          <p class="text-xs text-center text-white mt-4 ">
+          <p class="text-xs xl:text-sm text-center text-white mt-4 ">
             AutoCare is my go to for all my car maintenance. I’ve saved a lot of
             money that I would have lost at a mechanic’s shop.
           </p>
@@ -287,23 +284,23 @@
       </div>
     </div>
 
-    <div class="sm:px-20 lg:mt-48 lg:px-24 sm:mt-32 px-10 mt-20">
+    <div class="sm:px-20 lg:mt-48 lg:px-24 xl:px-36 sm:mt-32 px-10 mt-20">
       <h2
-        class="text-center text-nav-text archivo-font text-xl sm:text-3xl lg:text-3xl sm:w-9/12 sm:mx-auto"
+        class="text-center text-nav-text archivo-font text-xl sm:text-3xl lg:text-4xl xl:text-5xl sm:w-9/12 sm:mx-auto"
       >
         For just NGN 500
       </h2>
       <p
-        class="sm:mt-8 mt-5 text-sm text-center archivo-font text-body-text sm:text-lg sm:w-9/12 sm:mx-auto leading-tight"
+        class="sm:mt-8 mt-5 text-sm xl:text-xl text-center archivo-font text-body-text sm:text-lg sm:w-9/12 sm:mx-auto leading-tight"
       >
-        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore <br class="hidden xl:block">
         et dolore magna aliqua. Ut
       </p>
       <div
         class="sm:mt-8 lg:mt-12 mt-5 text-center archivo-font sm:w-9/12 sm:mx-auto"
       >
         <button
-          class="bg-scale-green py-2 rounded-lg px-5 focus:outline-none text-white"
+          class="bg-scale-green py-2 rounded-lg px-5 focus:outline-none text-white xl:text-2xl"
         >
           Download book now
         </button>
